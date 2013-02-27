9ed4b685855bba4c2a12d045ea2b0367c3ee5e33
Zhao Xinjie <369520295@qq.com>
2013-02-27 23:13:24 +0800

    样式修改

--- cgi/app.py
+++ cgi/app.py
@@ -8,11 +8,13 @@ urls = (
 )
 
 render = web.template.render('page/')
+db = web.database(dbn='mysql', user='todolist', pw='todolist_db_password', db='todolist')
 
 class Index:
     def GET(self):
-        i = web.input()
-        return render.index(i.name)
+        projects = db.select('Project');
+        todos = db.select('Item');
+        return render.index(projects, todos)
 
 app = web.application(urls,globals())
 
