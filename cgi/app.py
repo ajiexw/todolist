d9ccc164e00a46f1f19d77c687aa4b5962837be5
Zhao Xinjie <369520295@qq.com>
2013-03-02 23:56:15 +0800

    删除项目，修改任务

--- cgi/app.py
+++ cgi/app.py
@@ -5,8 +5,11 @@ import web
 
 urls = (
     '/','Index',
+    '/(\d*)','Project',
     '/add-project','AddProject', #添加项目
+    '/del-project/(\d*)','DelProject', #删除项目
     '/add-item','AddItem', #添加任务
+    '/update-item/(\d*)','UpdateItem', #修改任务
     '/del-item/(\d*)','DelItem', #删除任务
     '/finish-item/(\d*)','FinishItem', #任务完成
     '/unfinish-item/(\d*)','UnFinishItem', #返工任务
@@ -17,10 +20,16 @@ db = web.database(dbn='mysql', user='todolist', pw='todolist_db_password', db='t
 
 class Index:
     def GET(self):
+        web.seeother('/1')
+
+class Project:
+    def GET(self,project_id):
+        project_id = int(project_id)
         projects = db.select('Project');
-        todos = db.select('Item', where="done=0");
-        finish_todos = db.select('Item', where="done=1");
-        return render.index(projects, todos, finish_todos)
+        project = db.select('Project', where="Projectid=%s"%(project_id))[0]
+        todos = db.select('Item', where="done=0 and Projectid=%s"%(project_id))
+        finish_todos = db.select('Item', where="done=1 and Projectid=%s"%(project_id))
+        return render.index(projects, project, todos, finish_todos)
 
 class AddProject:
     def POST(self):
@@ -28,10 +37,22 @@ class AddProject:
         new_id = db.insert('Project',title=i.title)
         raise web.seeother('/')
 
+class DelProject:
+    def GET(self,project_id):
+        project_id = int(project_id)
+        db.delete('Project', where="Projectid=%s"%(project_id))
+        raise web.seeother('/')
+
 class AddItem:
     def POST(self):
         i = web.input()
-        new_id = db.insert('Item',content=i.content)
+        new_id = db.insert('Item',Projectid=i.Projectid, content=i.content)
+        raise web.seeother('/')
+
+class UpdateItem:
+    def POST(self, item_id):
+        i = web.input()
+        new_id = db.update('Item', where="Itemid=%s"%(item_id), content=i.content)
         raise web.seeother('/')
 
 class DelItem:
--- cgi/static/Page.js
+++ cgi/static/Page.js
@@ -9,14 +9,36 @@ function hideAddProject(){
     $('form[name=add-project]').hide();
 }
 
+//删除项目Project
+function confirmDeleteProject(project_id){
+    if(confirm('确认删除该项目？')){
+        window.location.href = '/del-project/'+project_id;
+    }
+}
+
 //删除任务item
-function confirmDelete(item_id){
+function confirmDeleteItem(item_id){
     if(confirm('确认删除该任务？')){
         window.location.href = '/del-item/'+item_id;
     }
 }
 
-//任务完成，嘎嘎！！
+//编辑任务item
+function editItem(this_item, item_id){
+    dd = $(this_item).parent();
+    $('.update_item').hide();
+    dd.children('.item_content, .checker, .edit_item, .del_item').hide();
+    dd.children('.update_item').show();
+    dd.children('.update_item').children('input.content').focus().keyup(function(){
+        if(event.keyCode==13){
+            $(this).parent().submit();
+        }
+    });
+
+
+}
+
+//任务完成
 function finishItem(item_id){
     window.location.href = '/finish-item/'+item_id;
 }
